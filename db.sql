fdfdabd8711021a8906b793d3ba105a1370206e9
King-pe <176069924+King-pe@users.noreply.github.com>
2026-03-02 03:43:27 -0500

    Add generated db.sql schema file

---
+++ db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "transactions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer,
+	"amount" integer NOT NULL,
+	"status" text DEFAULT 'pending' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
